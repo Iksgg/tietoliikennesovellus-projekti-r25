4e03a5964481d4a95a484609e5f7ec2626985008
Sami Ikalainen <100471282+Iksgg@users.noreply.github.com>
2025-11-06 10:48:50 +0200

    Alustava Kiihtyvysanturidata mittaus ja lähetys BLE yli

---
+++ code/Phase3/src/main.c
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gap.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/conn.h>
+#include <dk_buttons_and_leds.h>
+#include "my_lbs.h"
+#include "adc.h"
+
+static const struct bt_le_adv_param *adv_param = BT_LE_ADV_PARAM(
+	(BT_LE_ADV_OPT_CONN |
+	 BT_LE_ADV_OPT_USE_IDENTITY), /* Connectable advertising and use identity address */
+	800, /* Min Advertising Interval 500ms (800*0.625ms) */
+	801, /* Max Advertising Interval 500.625ms (801*0.625ms) */
+	NULL); /* Set to NULL for undirected advertising */
+
+LOG_MODULE_REGISTER(Lesson4_Exercise2, LOG_LEVEL_INF);
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+#define RUN_STATUS_LED  DK_LED1
+#define CON_STATUS_LED  DK_LED2
+#define USER_LED 		DK_LED3
+
+#define USER_BUTTON_1	DK_BTN1_MSK
+#define USER_BUTTON_2	DK_BTN2_MSK
+#define USER_BUTTON_3	DK_BTN3_MSK
+#define USER_BUTTON_4	DK_BTN4_MSK
+
+#define STACKSIZE 1024
+#define PRIORITY 7
+
+#define RUN_LED_BLINK_INTERVAL 1000
+#define NOTIFY_INTERVAL 500
+
+static bool app_button_state;
+static struct k_work adv_work;
+static struct Measurement app_sensor_data;
+static uint8_t current_pos = 0;
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LBS_VAL),
+};
+
+static void adv_work_handler(struct k_work *work)
+{
+	int err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+}
+
+static void advertising_start(void)
+{
+	k_work_submit(&adv_work);
+}
+
+static void recycled_cb(void)
+{
+	printk("Connection object available from previous conn. Disconnect is complete!\n");
+	advertising_start();
+}
+
+static void update_sensor_data(void)
+{
+	app_sensor_data = readADCValue();
+	app_sensor_data.pos = current_pos;
+}
+
+static void app_led_cb(bool led_state)
+{
+	dk_set_led(USER_LED, led_state);
+}
+
+static bool app_button_cb(void)
+{
+	return app_button_state;
+}
+
+void send_data_thread(void)
+{
+	while (1) {
+		/* Simulated ADC */
+		struct Measurement m;
+		m.x = 100;
+		m.y = 200;
+		m.z = 300;
+		m.pos = current_pos;
+
+		/* sensor data */
+		update_sensor_data();
+		/* Send notification, the function sends notifications only if a client is subscribed */
+		my_lbs_send_sensor_notify(&m);
+		k_sleep(K_MSEC(NOTIFY_INTERVAL));
+	}
+}
+
+static struct my_lbs_cb app_callbacks = {
+	.led_cb = app_led_cb,
+	.button_cb = app_button_cb,
+};
+
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	if ((has_changed & USER_BUTTON_1) && (button_state & USER_BUTTON_1)){
+		current_pos = 1;
+	}
+
+	if ((has_changed & USER_BUTTON_2) && (button_state & USER_BUTTON_2)){
+		current_pos = 2;
+	}
+
+	if ((has_changed & USER_BUTTON_3) && (button_state & USER_BUTTON_3)){
+		current_pos = 3;
+	}
+
+	if ((has_changed & USER_BUTTON_4) && (button_state & USER_BUTTON_4)){
+		current_pos = 4;
+	}
+}
+static void on_connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed (err %u)\n", err);
+		return;
+	}
+
+	printk("Connected\n");
+
+	dk_set_led_on(CON_STATUS_LED);
+}
+
+static void on_disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason %u)\n", reason);
+
+	dk_set_led_off(CON_STATUS_LED);
+}
+
+struct bt_conn_cb connection_callbacks = {
+	.connected = on_connected,
+	.disconnected = on_disconnected,
+	.recycled = recycled_cb,
+};
+
+static int init_button(void)
+{
+	int err;
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		printk("Cannot init buttons (err: %d)\n", err);
+	}
+	return err;
+}
+
+int main(void)
+{
+	int blink_status = 0;
+	int err;
+
+	LOG_INF("Starting Lesson 4 - Exercise 2 \n");
+
+	err = dk_leds_init();
+	if (err) {
+		LOG_ERR("LEDs init failed (err %d)\n", err);
+		return -1;
+	}
+
+	err = init_button();
+	if (err) {
+		printk("Button init failed (err %d)\n", err);
+		return -1;
+	}
+
+	err = bt_enable(NULL);
+	if (err) {
+		LOG_ERR("Bluetooth init failed (err %d)\n", err);
+		return -1;
+	}
+	bt_conn_cb_register(&connection_callbacks);
+
+	err = my_lbs_init(&app_callbacks);
+	if (err) {
+		printk("Failed to init LBS (err:%d)\n", err);
+		return -1;
+	}
+	LOG_INF("Bluetooth initialized\n");
+	k_work_init(&adv_work, adv_work_handler);
+	advertising_start();
+	for (;;) {
+		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
+		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+	}
+}
+
+K_THREAD_DEFINE(send_data_thread_id, STACKSIZE, send_data_thread, NULL, NULL, NULL, PRIORITY, 0, 0);

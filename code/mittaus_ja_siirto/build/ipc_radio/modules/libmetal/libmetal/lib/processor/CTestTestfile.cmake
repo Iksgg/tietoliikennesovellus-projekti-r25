68f58baafffd2251c65443a57c1b2d082edeae66
Sami Ikalainen <100471282+Iksgg@users.noreply.github.com>
2025-11-06 12:05:52 +0200

    poistettiin build directory seurannasta

--- code/mittaus_ja_siirto/build/ipc_radio/modules/libmetal/libmetal/lib/processor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ncs/v3.1.1/modules/hal/libmetal/libmetal/lib/processor
-# Build directory: C:/tietoliikennesovellus-projekti-r25/code/mittaus_ja_siirto/build/ipc_radio/modules/libmetal/libmetal/lib/processor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- code/mittaus_ja_siirto/build/mittaus_ja_siirto/modules/libmetal/libmetal/lib/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ncs/v3.1.1/modules/hal/libmetal/libmetal/lib
-# Build directory: C:/tietoliikennesovellus-projekti-r25/code/mittaus_ja_siirto/build/mittaus_ja_siirto/modules/libmetal/libmetal/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("compiler")
-subdirs("processor")
-subdirs("system")

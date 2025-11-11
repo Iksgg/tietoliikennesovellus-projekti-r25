0807c3938fea008587e0431f551454a27afe0a7c
Valtteri Hietamäki <noname@example.com>
2025-11-11 11:36:33 +0200

    Tehty python script datan lähettämiseen

--- code/bleaktesti/bleakclient.py
+++ code/bleaktesti/bleakclient.py
@@ -2,14 +2,40 @@ import asyncio
 
 from bleak import BleakClient
 from bleak.exc import BleakCharacteristicNotFoundError
+import mysql.connector
+import struct
 
 char_uuid = "00001526-1212-EFDE-1523-785FEABCD123"
-address = "D7:38:E3:1C:21:EF" 
+address = "CF:01:6d:B4:C3:83" 
+raspPi_mac ="B8:27:EB:9D:BC:0E"
 
 
 def notif_callback(sender, data: bytearray):
+    x,y,z,pos = struct.unpack('<HHHB',data)
+    print(x,y,z,pos)
     print(f"Lähetetty data: {data}")
+    loop = asyncio.get_event_loop()
+    loop.run_in_executor(None, send_data, x, y, z, pos, sender, raspPi_mac)
 
+
+
+def send_data(x, y, z, pos, from_mac, to_mac, groupid=25):
+    conn = mysql.connector.connect(
+        host = "172.20.241.9",
+        user = "dbaccess_rw",
+        password = "fasdjkf2389vw2c3k234vk2f3",
+        database = "measurements",
+        port = 3306
+    )
+    try:
+        cur = conn.cursor()
+        query = "INSERT INTO measurements (groupid, from_mac, to_mac, sensorvalue_a, sensorvalue_b, sensorvalue_c, sensorvalue_d) VALUES (%s,%s,%s,%s,%s,%s,%s) "
+        params = (groupid, from_mac, to_mac, x, y, z, pos)
+        cur.execute(query, params)
+        conn.commit()
+        cur.close()
+    finally:
+        conn.close()
 
 async def main():
     try:
@@ -18,7 +44,7 @@ async def main():
             print(f"Services: {client.services}")
             await client.start_notify(char_uuid, notif_callback)
             print("Listening for notifications...")
-            await asyncio.sleep(60)  # Kuuntele 30 sekuntia
+            await asyncio.sleep(60)  
             await client.stop_notify(char_uuid)
     except BleakCharacteristicNotFoundError:
         print("Characteristic not found")

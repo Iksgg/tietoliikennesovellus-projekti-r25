12f8fc28c8ef1c8c3834597847fbefd602c8349c
Valtteri Hietamäki <noname@example.com>
2025-11-12 11:06:28 +0200

    Debugattu envit

--- code/bleaktesti/bleakclient.py
+++ code/bleaktesti/bleakclient.py
@@ -3,8 +3,10 @@ from bleak import BleakClient
 from bleak.exc import BleakCharacteristicNotFoundError
 import mysql.connector
 import struct
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 import os
+
+load_dotenv()
 
 db_host = os.getenv("DB_HOST")
 db_user = os.getenv("DB_USER")
